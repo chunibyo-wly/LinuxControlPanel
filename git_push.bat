c16216101cc20c2fbc9c9e273683a68866871ee2
chunibyo-wly <359806334@qq.com>
2020-05-15 18:45:38 +0800

    --story=$1 --user=鍚撮緳姘?

--- git_push.bat
+++ git_push.bat
@@ -1,3 +1,3 @@
 git add .
 git commit --allow-empty -m "--story="$1" --user=吴龙永"
-git push origin iteration-one
+git push origin dev-one
--- git_push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git add .
-git commit --allow-empty -m "--story="$1" --user=吴龙永"
-git push origin iteration-one
