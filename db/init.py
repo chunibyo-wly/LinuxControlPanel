7689789a86327ebeee3fde32fd79b9c87b7fa5c5
chunibyo-wly <chunibyo.wly@gmail.com>
2020-05-22 19:40:25 +0800

    --story=0 --user=吴龙永

--- db/init.py
+++ db/init.py
@@ -1,11 +1,9 @@
 import sqlite3
 
 conn = sqlite3.connect('db/Linux.db')
-print("数据库连接(创建)成功")
 cursor = conn.cursor()
 
 cursor.execute(open('db/init.sql', 'r').read())
-print("数据库初始化成功")
 
 conn.commit()
 conn.close()
