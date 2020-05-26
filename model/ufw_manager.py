373434c12f9af832a3fc530d23ccac913d889275
chunibyo-wly <chunibyo.wly@gmail.com>
2020-05-26 22:30:29 +0800

    --story=1003246 --user=陈彦兵

---
+++ model/ufw_manager.py
@@ -0,0 +1,4 @@
+class UFWManager:
+    def __init__(self, port=80, ip=""):
+        self.port = port
+        self.ip = ip
---
+++ script/install_ufw.sh
@@ -0,0 +1,5 @@
+sudo apt install ufw
+
+ufw allow ssh
+ufw allow 8888
+ufw enable
