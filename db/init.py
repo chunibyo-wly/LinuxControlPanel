a28bf56beeea21f197f80099bbcc57adc5ca6c13
chunibyo-wly <chunibyo.wly@gmail.com>
2020-05-29 08:56:31 +0800

    --story=1002714 --user=陈彦兵

--- db/init.py
+++ db/init.py
@@ -1,10 +1,9 @@
+import os
 import sqlite3
-import sys, os
+import sys
 
 sys.path.append(os.path.join(os.getcwd()))
 print(sys.path)
-
-from model.ufw_manager import PortManager
 
 conn = sqlite3.connect('db/Linux.db')
 cursor = conn.cursor()
@@ -13,18 +12,3 @@ cursor.executescript(open('db/init.sql', 'r').read())
 
 conn.commit()
 conn.close()
-
-result = PortManager.get_allow_port()
-
-flagA = True
-flagB = True
-for i in result:
-    if i['port'] == 22:
-        flagA = False
-    elif i['port'] == 8888:
-        flagB = False
-
-if flagA:
-    PortManager(22, 'tcp').add_allow_port('ssh')
-if flagB:
-    PortManager(8888, 'tcp').add_allow_port('web')
--- script/install_ufw.sh
+++ script/install_ufw.sh
@@ -1,5 +1,5 @@
 sudo apt install ufw
 
-# ufw allow ssh
-# ufw allow 8888
+ufw allow ssh
+ufw allow 8888
 echo y | sudo ufw enable
